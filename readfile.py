f84012d2183b14aaf90ba6ec7970827b53fb6fac
HalleYi <154717875+HalleyLab@users.noreply.github.com>
2026-03-10 15:05:05 -0400

    Delete readfile.py

--- readfile.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-def read(fileName, category):
-    data = pd.read_excel(fileName)
-    questions = {k: v for k, v in zip(data[data['类型']==category]['题目'].tolist(), data[data['类型']==category]['参考答案'].tolist())}
-    return questions
