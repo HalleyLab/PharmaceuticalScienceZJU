c0e47cce4b9987341cc0a7f12ced8f5f721e02e8
HalleYi <ZJUHalleY@outlook.com>
2026-06-30 14:27:59 -0400

    Remove rotating home avatar

--- web/js/definition.js
+++ web/js/definition.js
@@ -578,87 +578,23 @@
   }); 
 })(jQuery);
 
-const homeAvatarImages = [
-  './photos/德克萨斯（三丽鸥）.JPG',
-  './photos/德克萨斯（电瓶）.jpg',
-  './photos/德克萨斯（生无可恋）.JPG',
-  './photos/桃金娘（投降）.jpg'
-];
-
-let homeAvatarIndex = 0;
-let homeAvatarTimer = null;
-let homeAvatarResumeTimer = null;
-
 function setHomeAvatar(src) {
   const image = document.getElementById('home_img');
   if (!image) {
     return;
   }
-
-  image.classList.add('is-changing');
-  window.setTimeout(function() {
-    image.src = src;
-    image.classList.remove('is-changing');
-  }, 220);
-}
-
-function showNextHomeAvatar() {
-  homeAvatarIndex = (homeAvatarIndex + 1) % homeAvatarImages.length;
-  setHomeAvatar(homeAvatarImages[homeAvatarIndex]);
-}
-
-function startHomeAvatarCarousel() {
-  if (homeAvatarTimer) {
-    window.clearInterval(homeAvatarTimer);
-  }
-  homeAvatarTimer = window.setInterval(showNextHomeAvatar, 4200);
-}
-
-function pauseHomeAvatarCarousel() {
-  if (homeAvatarTimer) {
-    window.clearInterval(homeAvatarTimer);
-    homeAvatarTimer = null;
-  }
-  if (homeAvatarResumeTimer) {
-    window.clearTimeout(homeAvatarResumeTimer);
-    homeAvatarResumeTimer = null;
-  }
-}
-
-function resumeHomeAvatarCarousel(delay) {
-  pauseHomeAvatarCarousel();
-  homeAvatarResumeTimer = window.setTimeout(function() {
-    setHomeAvatar(homeAvatarImages[homeAvatarIndex]);
-    startHomeAvatarCarousel();
-  }, delay);
+  image.src = src;
 }
 
 function change_img(site) {
   if (site == 'wechat') {
-    pauseHomeAvatarCarousel();
     setHomeAvatar('./img/wechat.jpg');
-    resumeHomeAvatarCarousel(7000);
   } else if (site == 'qq') {
-    pauseHomeAvatarCarousel();
     setHomeAvatar('./img/qq.jpg');
-    resumeHomeAvatarCarousel(7000);
   } else if (site == 'outlook') {
-    pauseHomeAvatarCarousel();
     setHomeAvatar('./img/outlook.png');
-    resumeHomeAvatarCarousel(7000);
   } else if (site == 'home') {
-    pauseHomeAvatarCarousel();
-    setHomeAvatar(homeAvatarImages[homeAvatarIndex]);
-    startHomeAvatarCarousel();
+    setHomeAvatar('./img/texas_blank.png');
   }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  const image = document.getElementById('home_img');
-  if (!image || !homeAvatarImages.length) {
-    return;
-  }
-  image.src = homeAvatarImages[homeAvatarIndex];
-  startHomeAvatarCarousel();
-});
 
